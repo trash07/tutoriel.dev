258a703df157c4a2ea02c32a91fbe122f6276bfc
trash07 <mmkpeto5@gmail.com>
2026-02-22 18:59:02 -0500

    feat: add gatsby files

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `tutoriel.dev`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
