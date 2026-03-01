18de0173259c1d107d649f4aaa23b860c9859d59
trash07 <mmkpeto5@gmail.com>
2026-03-01 13:24:15 -0500

    feat: basic post layout

--- src/pages/blog-posts/{mdx.frontmatter__slug}.jsx
+++ src/pages/blog-posts/{mdx.frontmatter__slug}.jsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import { PostLayout } from '../../components/PostLayout';
 
-const PostPage = (props) => {
-  return <>{props.children}</>;
+const PostPage = ({ pageContext, children }) => {
+  const { frontmatter } = pageContext;
+  return <PostLayout title={frontmatter.title}>{children}</PostLayout>;
 };
 
 export default PostPage;
+
+export const Head = ({ pageContext }) => {
+  const { frontmatter } = pageContext;
+  return <title>{frontmatter.title}</title>;
+};
