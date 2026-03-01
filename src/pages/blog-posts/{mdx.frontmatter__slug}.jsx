8d7f9f8a5f6a62d045daf24a0cdf0d86d884e8e9
trash07 <mmkpeto5@gmail.com>
2026-03-01 13:33:48 -0500

    feat: update page layout

--- src/pages/blog-posts/{mdx.frontmatter__slug}.jsx
+++ src/pages/blog-posts/{mdx.frontmatter__slug}.jsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { PostLayout } from '../../components/PostLayout';
 
-const PostPage = ({ pageContext, children }) => {
-  const { frontmatter } = pageContext;
-  return <PostLayout title={frontmatter.title}>{children}</PostLayout>;
-};
-
-export default PostPage;
-
 export const Head = ({ pageContext }) => {
   const { frontmatter } = pageContext;
   return <title>{frontmatter.title}</title>;
 };
+
+export default function PostPage({ pageContext, children }) {
+  const { frontmatter } = pageContext;
+  return <PostLayout title={frontmatter.title}>{children}</PostLayout>;
+}
